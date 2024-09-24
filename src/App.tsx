a2ec609b54687e2aa881e189199f28f228091284
Juan Correa <jcorrea22@cuc.edu.co>
2024-09-24 12:47:25 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import milogo from "../src/assets/img/RomanReingsOTC.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="RomanReings" />
+        <h1>THE ORIGINAL TRIBAL CHIEF ROMAN REINGS</h1>
+        <p>
+          Este es el luchador mas importante de la nueva generacion de WWE
+          siendo la atraccion taquillera mas grande desde el año 2015 al
+          2024..., en su palmares cuenta con 5 titulos mundiales siendo el
+          ultimo el reinado mas largo en la era moderna el cual tuvo una
+          duracion de 1316 dias de reinado...
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
